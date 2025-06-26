54ee111d0983651a534a85f2fb306b0cd7009ba6
Luciano Figueroa <lumusitech@gmail.com>
2025-06-26 19:16:28 -0300

    chore: add DB and log config

--- src/main/java/com/lumusitech/zona_fit/ZonaFitApplication.java
+++ src/main/java/com/lumusitech/zona_fit/ZonaFitApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZonaFitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZonaFitApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZonaFitApplication.class, args);
+    }
 
 }
